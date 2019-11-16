f3f6a97f28c985e4bae308b17853fdada5c902dd
nicos <nicolas.turck@gmail.com>
2019-11-16 18:34:35 +0100

    write imports methods in main and create function wheel_result in functions

---
+++ fonctions.py
@@ -0,0 +1,7 @@
+from random import * # i import the random method with my function wheel_result()
+from math import ceil # i import the method ceil of module math for my function compare()
+
+def wheel_result():
+    """i create a function for choose a random number"""
+    wheel = randrange(0, 50)
+    return wheel # i return the result of my function
---
+++ main.py
@@ -0,0 +1,6 @@
+from functions import * # i import all function of files functions.py
+from random import * # i import the method random for random choice wheel
+from math import ceil # i import the method ceil of math module to round the bet and the gain
+
+print("welcome to the casino")
+gain = 1000 # i declare the gain of departure is 1000$
